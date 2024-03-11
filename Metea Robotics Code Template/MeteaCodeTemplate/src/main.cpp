f49c3c8feb1c57b93d06b966260d786cc6d090cd
Ayush Chandra <ayushchandra8907@gmail.com>
2024-03-11 14:20:44 -0500

    added metea code/comp template

---
+++ Metea Robotics Code Template/MeteaCodeTemplate/src/main.cpp
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\ayush                                            */
+/*    Created:      Mon Mar 11 2024                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// RFM                  motor         1               
+// RMM                  motor         2               
+// RBM                  motor         3               
+// LFM                  motor         4               
+// LMM                  motor         5               
+// LBM                  motor         6               
+// cata                 motor         7               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
