a6958560a18947d1545e6be58398f4896ba79f1f
Ayush Chandra <71286983+ayushchandra8907@users.noreply.github.com>
2025-08-31 14:02:46 -0500

    Delete ScienceAssessment directory

--- ScienceAssessment/test.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-root = Tk()
-canvas = Canvas(root, width = 833, height = 649)
-canvas.pack()
-img = PhotoImage(file="Question1Science.gif")
-canvas.create_image(20,20, anchor=NW, image=img)
-mainloop()
